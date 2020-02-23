9f8284e1df1c99831ff8e7c189c64905a801d58d
Niels Nijens <nijens.niels@gmail.com>
2020-02-23 22:04:56 +0100

    Add dependency injection and configuration classes for the bundle

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the niels-nijens/sculpin-contentful-bundle package.
+ *
+ * (c) Niels Nijens <nijens.niels@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nijens\SculpinContentfulBundle\DependencyInjection;
+
+use Contentful\ContentfulBundle\DependencyInjection\Configuration as ContentfulConfiguration;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Validates and merges configuration from the configuration files.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+class Configuration extends ContentfulConfiguration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('nijens_sculpin_contentful');
+
+        $rootNode = $treeBuilder->getRootNode();
+        $rootNode->append(parent::getConfigTreeBuilder()->getRootNode());
+
+        return $treeBuilder;
+    }
+}
---
+++ src/DependencyInjection/NijensSculpinContentfulExtension.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the niels-nijens/sculpin-contentful-bundle package.
+ *
+ * (c) Niels Nijens <nijens.niels@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nijens\SculpinContentfulBundle\DependencyInjection;
+
+use Contentful\ContentfulBundle\ContentfulBundle;
+use Contentful\ContentfulBundle\DependencyInjection\ContentfulExtension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * Loads the services and configuration into the service container.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+final class NijensSculpinContentfulExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration($container->getParameter('kernel.debug'));
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.xml');
+
+        $this->loadContentfulExtension($configs, $container);
+    }
+
+    /**
+     * Loads the extension from the {@see ContentfulBundle}.
+     */
+    private function loadContentfulExtension(array $configs, ContainerBuilder $container): void
+    {
+        $configs = array_map(
+            function (array $config) {
+                return $config['contentful'];
+            },
+            $configs
+        );
+
+        $contentfulExtension = new ContentfulExtension();
+        $contentfulExtension->load($configs, $container);
+    }
+}
